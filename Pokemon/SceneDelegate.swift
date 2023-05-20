1863468dd9c53b8285027ff4ef28cbaef25524fa
adrianorezena <adrianocr@gmail.com>
2023-05-20 08:28:41 -0300

    removed Main.storyboard

--- Pokemon/SceneDelegate.swift
+++ Pokemon/SceneDelegate.swift
@@ -11,7 +11,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        navigationController.navigationBar.prefersLargeTitles = true
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
 }
