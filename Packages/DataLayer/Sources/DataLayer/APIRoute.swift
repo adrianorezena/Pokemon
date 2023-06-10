37710424702f9d29d1a91e7f97c0591e2e7b7cca
adrianorezena <adrianocr@gmail.com>
2023-06-10 09:33:37 -0300

    move targets to local packages

---
+++ Packages/DataLayer/Sources/DataLayer/APIRoute.swift
@@ -0,0 +1,43 @@
+//
+//  APIRoute.swift
+//  DataLayer
+//
+//  Created by Adriano Rezena on 20/05/23.
+//
+
+import Foundation
+
+struct APIRouteError: Error {}
+
+enum APIRoute {
+    case getSpeciesList(limit: Int, offset: Int)
+    case getSpecies(id: String)
+    case getEvolutionChain(id: String)
+
+    private var baseURLString: String { "https://pokeapi.co/api/v2/" }
+
+    var url: URL? {
+        switch self {
+        case .getSpecies(let id):
+            return URL(string: baseURLString + "pokemon-species/" + id)
+            
+        case .getEvolutionChain(let id):
+            return URL(string: baseURLString + "evolution-chain/" + id)
+            
+        case let .getSpeciesList(limit, offset):
+            guard var components: URLComponents = URLComponents(string: baseURLString + "pokemon-species") else {
+                return nil
+            }
+            
+            let queryItems: [URLQueryItem] = [
+                URLQueryItem(name: "limit", value: String(limit)),
+                URLQueryItem(name: "offset", value: String(offset))
+            ]
+            
+            components.queryItems?.append(contentsOf: queryItems)
+            
+            return components.url
+        }
+    }
+    
+}
